c4d476772e0f757a0822c3a1e015ef0185067d06
rudrashan16 <74367420+rudrashan16@users.noreply.github.com>
2021-09-05 00:00:34 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var maze;
+
+function preload() {
+ 
+  
+  
+  
+}
+
+function setup() {
+  createCanvas(1000,500);
+  var maze=createSprite(100,100,100,10) 
+
+  
+}
+
+function draw() {
+  background();
+  
+  drawSprites();
+}
